ec0f8044e97c621ad0ac899f82c5b28837cfbfb9
Koushik Samanta <koushik.engine@gmail.com>
2023-01-30 08:26:40 +0530

    update

--- src/app/components/body/battery-test/create-new-test/FormFields.ts
+++ src/app/components/body/battery-test/create-new-test/FormFields.ts
@@ -1,54 +1,218 @@
-let testMethods =[
-    {
-        name:'Charge/discharge experiment',
-        fields:[
-            {name:'Channel Number',type:'number',disabled:false},
-            {name:'Test Number',type:'text',disabled:true},
-            {name:'Test Name',type:'text',desabled:false},
-            {name:'Operating Temperature (°C)',type:'number',disabled:false},
-            {name:'Max-voltage (V)',type:'number',disabled:false},
-            {name:'Min-voltage (V)',type:'number',disabled:false},
-            {name:'Constant current rate (A)',type:'number',disabled:false},
-            {name:'Test Description',type:'textarea',disabled:false}
+export class SelectField {
+  value?: any;
+  type: string = 'select';
+  constructor(
+    public id: number,
+    public options: any[],
+    public visibility: boolean = true,
+    public template_width: number = 1
+  ) {
+    this.value = this.options[0];
+  }
+}
+export class TextField {
+  type: string = 'text';
+  constructor(
+    public id: number,
+    public value: 'at' | 'for',
+    public visibility: boolean = true,
+    public template_width: number = 1
+  ) {}
+}
+export class InputField {
+  type: string = 'input';
+  constructor(
+    public id: number,
+    public value: any = null,
+    public visibility: boolean = true,
+    public template_width: number = 1
+  ) {}
+}
+export interface Fields {
+  id: number;
+  fields: (SelectField | TextField | InputField)[];
+  mulitiplier: number;
+}
+export class TestFormat {
+  children: Fields[] = [];
+  constructor(public name: string, public value: number) {}
+}
+const testFormats: any = [
+  {
+    name: 'Do this for this long.',
+    value: 1,
+    children: [
+      {
+        id: 1,
+        fields: [
+          {
+            id: 1,
+            type: 'select',
+            options: ['Charge', 'Discharge', 'Rest', 'Hold'],
+            value: 'Charge',
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 2,
+            type: 'text',
+            value: 'at',
+            visibility: true,
+            template_width: 1,
+          },
+          {
+            id: 3,
+            type: 'input',
+            value: null,
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 4,
+            type: 'select',
+            options: ['C', 'A', 'W'],
+            value: 'C',
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 5,
+            type: 'text',
+            value: 'for',
+            visibility: true,
+            template_width: 1,
+          },
+          {
+            id: 6,
+            type: 'input',
+            value: null,
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 7,
+            type: 'select',
+            options: ['hours.', 'minutes.', 'seconds.'],
+            value: 'hours.',
+            visibility: true,
+            template_width: 2,
+          },
         ],
-        labels:[
-            'Set the appropriate channel on which you want to carry out experiment',
-            'It gets generated automatically.',
-            'Put a name which is meaningful to you.',
-            'Provide the temperature (in °C) at which you want to carry out your test.',
-            'Set the maximum voltage (in Volt) for the cell. Usually, it is provided by the manufacturer.',
-            'Set the cut-off voltage (in Volt) for the cell. Usually, specified by the manufacturer',
-            'Set the current (in Amps) at which you want to carry out the charging and discharging.',
-            ''
-        ]
-    },
-    {
-        name:'Pulse charging',
-        fields:[
-            {name:'Channel Number',type:'number',disabled:false},
-            {name:'Test Number',type:'text',disabled:true},
-            {name:'Test Name',type:'text',desabled:false},
-            {name:'Operating Temperature (°C)',type:'number',disabled:false},
-            {name:'Max-voltage (V)',type:'number',disabled:false},
-            {name:'Min-voltage (V)',type:'number',disabled:false},
-            {name:'Average current rate (A)',type:'number',disabled:false},
-            {name:'Frequency (Hz)',type:'number',disabled:false},
-            {name:'Duty ratio (%)',type:'number',disabled:false},
-            {name:'Test Description',type:'textarea',disabled:false}
+        multiplier: 1,
+      },
+    ],
+  },
+  {
+    name: 'Do this until this happen.',
+    value: 2,
+    children: [
+      {
+        id: 1,
+        fields: [
+          {
+            id: 1,
+            type: 'select',
+            options: ['Charge', 'Discharge'],
+            value: 'Charge',
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 2,
+            type: 'text',
+            value: 'at',
+            visibility: true,
+            template_width: 1,
+          },
+          {
+            id: 3,
+            type: 'input',
+            value: null,
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 4,
+            type: 'select',
+            options: ['C', 'A', 'W'],
+            value: 'C',
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 5,
+            type: 'text',
+            value: 'until',
+            visibility: true,
+            template_width: 1,
+          },
+          {
+            id: 6,
+            type: 'input',
+            value: null,
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 7,
+            type: 'text',
+            value: 'volt.',
+            visibility: true,
+            template_width: 2,
+          },
         ],
-        labels:[
-            'Set the appropriate channel on which you want to carry out experiment',
-            'It gets generated automatically.',
-            'Put a name which is meaningful to you.',
-            'Provide the temperature (in °C) at which you want to carry out your test.',
-            'Set the maximum voltage (in Volt) for the cell. Usually, it is provided by the manufacturer.',
-            'Set the cut-off voltage (in Volt) for the cell. Usually, specified by the manufacturer',
-            'Set the current (in Amps) at which you want to carry out the charging and discharging.',
-            'Set the frequency (in Hz)  for charging/rest period.',
-            'Set the duty ratio (in percent) for charging/rest period.',
-            ''
-        ]
-    }
-]
+        multiplier: 1,
+      },
+    ],
+  },
+  {
+    name: 'Run X.',
+    value: 3,
+    children: [
+      {
+        id: 1,
+        fields: [
+          {
+            id: 1,
+            type: 'text',
+            value: 'Run',
+            visibility: true,
+            template_width: 1,
+          },
+          {
+            id: 2,
+            type: 'input',
+            value: null,
+            visibility: true,
+            template_width: 2,
+          },
+          {
+            id: 3,
+            type: 'text',
+            value: 'for',
+            visibility: true,
+            template_width: 1,
+          },
+          {
+            id: 4,
+            type: 'input',
+            value: null,
+            visibility: true,
+            template_width: 3,
+          },
+          {
+            id: 5,
+            type: 'select',
+            options: ['seconds.', 'kms.'],
+            value: 'seconds.',
+            visibility: true,
+            template_width: 2,
+          },
+        ],
+        multiplier: 1,
+      },
+    ],
+  },
+];
 
-export {testMethods}
+export { testFormats };
--- src/app/components/body/battery-test/create-new-test/create-new-test.component.ts
+++ src/app/components/body/battery-test/create-new-test/create-new-test.component.ts
@@ -1,28 +1,85 @@
 import { Component, OnInit } from '@angular/core';
-import { testMethods } from './FormFields';
+import {
+  testFormats,
+  TestFormat,
+  SelectField,
+  TextField,
+  InputField,
+  Fields,
+} from './FormFields';
 
 @Component({
   selector: 'app-create-new-test',
   templateUrl: './create-new-test.component.html',
-  styleUrls: ['./create-new-test.component.css']
+  styleUrls: ['./create-new-test.component.css'],
 })
 export class CreateNewTestComponent implements OnInit {
-  _testMethods:any = testMethods;
-  selectedTestMethod?:any;
-  selectedTestMethodParameters?:any ={};
-  constructor() { }
+  selectedTestFormat?:any = [];
+  conAmTe: boolean = true;
+  _allTestFormats:any = []
+
+  constructor() {
+  }
 
   ngOnInit(): void {
-    this.selectedTestMethod = this._testMethods[0];
+    this.addRow();
   }
+  addRow(){
+    this._allTestFormats.push([...testFormats]);
+    let currentSelectedFormat = this._allTestFormats[this._allTestFormats.length-1][0];
+    this.selectedTestFormat.push(currentSelectedFormat);
+  }
+  save() {
+    console.log(this.selectedTestFormat);
+  }
+  update() {}
+  fieldResolve(field_id: number, child_id: number, row_id:number) {
+    let currentTestFormat = this.selectedTestFormat[row_id]
+    // if (currentTestFormat.id == 1) {
+    //   if (field_id == 1) {
+    //     if (
+    //       currentTestFormat.children[child_id - 1].fields[0].value ==
+    //       'Rest'
+    //     ) {
+    //       currentTestFormat.children[child_id - 1].fields[1].visibility =
+    //         false; //at
+    //       currentTestFormat.children[child_id - 1].fields[2].visibility =
+    //         false; //value
+    //       currentTestFormat.children[child_id - 1].fields[3].visibility =
+    //         false; //C|A|W
+    //     } else {
+    //       currentTestFormat.children[child_id - 1].fields[1].visibility =
+    //         true; //at
+    //       currentTestFormat.children[child_id - 1].fields[2].visibility =
+    //         true; //value
+    //       currentTestFormat.children[child_id - 1].fields[3].visibility =
+    //         true; //C|A|W
+    //       if (
+    //         currentTestFormat.children[child_id - 1].fields[0].value ==
+    //         'Hold'
+    //       ) {
+    //         currentTestFormat.children[child_id - 1].fields[3].options = [
+    //           'V',
+    //         ];
+    //         currentTestFormat.children[child_id - 1].fields[3].value =
+    //           'V';
+    //       } else {
+    //         currentTestFormat.children[child_id - 1].fields[3].options = [
+    //           'C',
+    //           'A',
+    //           'W',
+    //         ];
+    //         currentTestFormat.children[child_id - 1].fields[3].value =
+    //           'C';
+    //       }
+    //     }
+    //   }
+    // }
+    // else if(currentTestFormat.id == 2) {
 
-  save(){
-  }
-  update(){
-    for(let i of this.selectedTestMethod.fields){
-      this.selectedTestMethodParameters[i] = null;
-    }
-    return this.selectedTestMethod;
-  }
+    // }
+    // else{
 
+    // }
+  }
 }

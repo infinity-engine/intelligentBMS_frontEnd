d76fcba7714b67214ae84b878025a3cb0f231397
Samanta <koushik.engine@gmail.com>
2023-02-21 15:11:01 +0530

    update

--- src/app/components/body/devices/devices.component.ts
+++ src/app/components/body/devices/devices.component.ts
@@ -1,19 +1,34 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { trigger,state,style,animate,transition } from '@angular/animations';
 
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
-  styleUrls: ['./devices.component.css']
+  styleUrls: ['./devices.component.css'],
+  animations:[
+    trigger('insertRemoveTrigger',[
+      transition(':enter', [
+        style({ transform: 'translateX(20%)'}),
+        animate('100ms', style({ transform: 'translateX(0%)'})),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ transform: 'translateX(20%)'}))
+      ])
+    ])
+  ]
 })
 export class DevicesComponent implements OnInit {
-
-  constructor(private location:Location) { }
+  words=["test chambers.","cells.","battery packs."];
+  currentWord = 0;
+  constructor(private location: Location) {}
 
   ngOnInit(): void {
-    if(window.location.hostname != 'localhost'){
-    this.location.replaceState('./');//on prod
+    if (window.location.hostname != 'localhost') {
+      this.location.replaceState('./'); //on prod
     }
+    setInterval(()=>{
+      this.currentWord = (this.currentWord+1)%this.words.length;
+    },3000)
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -9,11 +10,12 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     FormsModule,
-
+    BrowserAnimationsModule
   ],
   exports:[
     CommonModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ]
 })
 export class SharedModule { }

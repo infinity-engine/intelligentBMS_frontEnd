1c7849fe61b5d35cffdc6f3359b584b1416e5671
Koushik Samanta <koushik.engine@gmail.com>
2023-03-25 17:51:04 +0530

    doc update

--- src/app/components/body/docs/docs.component.ts
+++ src/app/components/body/docs/docs.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { ComponentStoreService } from './../../../services/component-store.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-docs',
   templateUrl: './docs.component.html',
-  styleUrls: ['./docs.component.css']
+  styleUrls: ['./docs.component.css'],
 })
-export class DocsComponent implements OnInit {
-
-  constructor() { }
+export class DocsComponent implements OnInit, OnDestroy {
+  constructor(private _store: ComponentStoreService) {}
 
   ngOnInit(): void {
+    this._store.changeStyleForDoc(true);
   }
-
+  ngOnDestroy(): void {
+    this._store.changeStyleForDoc(false);
+  }
 }

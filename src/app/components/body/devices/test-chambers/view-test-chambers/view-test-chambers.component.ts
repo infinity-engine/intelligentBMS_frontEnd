2dc829f810df85fab22edd567835d493fe837a31
Samanta <koushik.engine@gmail.com>
2023-03-29 17:29:45 +0530

    update

--- src/app/components/body/devices/test-chambers/view-test-chambers/view-test-chambers.component.ts
+++ src/app/components/body/devices/test-chambers/view-test-chambers/view-test-chambers.component.ts
@@ -1,8 +1,9 @@
+import { ComponentStoreService } from './../../../../../services/component-store.service';
 import { Subscription } from 'rxjs';
 import { TestChamberService } from 'src/app/services/test-chamber.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { _TestChamber } from 'src/app/models/TestChamber';
-
+import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-view-test-chambers',
   templateUrl: './view-test-chambers.component.html',
@@ -17,8 +18,13 @@ export class ViewTestChambersComponent implements OnInit, OnDestroy {
   searchStr: string = '';
   selectedTestChamber: _TestChamber | undefined = undefined;
   placeHolderMsg: string = "You don't have any Test Chambers.";
+  isApiVisible: boolean = false;
 
-  constructor(private _testChamberService: TestChamberService) {}
+  constructor(
+    private _testChamberService: TestChamberService,
+    private clipboard: Clipboard,
+    private componentStoreService: ComponentStoreService
+  ) {}
   ngOnInit(): void {
     this.placeHolderMsg = 'Getting the information...';
     const sub = this._testChamberService.getChambers().subscribe({
@@ -67,8 +73,36 @@ export class ViewTestChambersComponent implements OnInit, OnDestroy {
       this.selectedTestChamber = this.availableTestChambers.find(
         (chamber) => chamber._id == chamberId
       );
+      if (this.selectedTestChamber) {
+        this.selectedTestChamber.apiKey = '**********';
+      }
     } catch {
       this.selectedTestChamber = undefined;
     }
+  }
+  changeView() {
+    this.isApiVisible = !this.isApiVisible;
+    if (this.isApiVisible) {
+      const sub = this._testChamberService
+        .getAPIKey(this.selectedTestChamber?._id as any)
+        .subscribe((api: any) => {
+          if (this.selectedTestChamber) {
+            this.selectedTestChamber.apiKey = api.apiKey;
+          }
+        });
+      this.subs.push(sub);
+    } else {
+      if (this.selectedTestChamber)
+        this.selectedTestChamber.apiKey = '**********';
+    }
+  }
+  clipBoard() {
+    if (this.selectedTestChamber)
+      this.clipboard.copy(this.selectedTestChamber.apiKey as any);
+    this.componentStoreService.sendToastMsg({
+      msg: 'API Key copied to clipboard',
+      timeOut: 2000,
+      color: 'black',
+    });
   }
 }
